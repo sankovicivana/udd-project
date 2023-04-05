070bf7b8672b42ec41c012413836736e3d8dc40b
Ivana <67102046+sankovicivana@users.noreply.github.com>
2023-04-05 17:58:05 +0200

    Init project
    Add Spring Framework

---
+++ src/main/java/ftn/project/udd/UddApplication.java
@@ -0,0 +1,13 @@
+package ftn.project.udd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ftn/project/udd/UddApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.project.udd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
